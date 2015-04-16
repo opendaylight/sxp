71f655a0cac6b813d30e4f32f540c783e91578d7
Maros Marsalek <mmarsale@cisco.com>
2015-04-16 14:55:37 +0200

    BUG-2918 Send binding replacements when deleting bindings in listeners
    If a binding gets deleted, notify listeners but also include its replacement
    binding (if present) in the update message.
    
    Change-Id: I8570f62fbb66837eb8b32bdccfa75b24bfb1d97c
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- sxp-core/src/main/java/org/opendaylight/sxp/util/database/MasterBindingIdentity.java
+++ sxp-core/src/main/java/org/opendaylight/sxp/util/database/MasterBindingIdentity.java
@@ -8,10 +8,18 @@
 
 package org.opendaylight.sxp.util.database;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
+import java.util.Map;
 import org.opendaylight.sxp.util.inet.IpPrefixConv;
+import org.opendaylight.sxp.util.time.TimeConv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev141002.DatabaseAction;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev141002.master.database.fields.Source;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev141002.master.database.fields.SourceBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev141002.master.database.fields.source.PrefixGroup;
@@ -21,12 +29,18 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev141002.mast
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev141002.sxp.databases.fields.MasterDatabase;
 
 public class MasterBindingIdentity {
+
+    private boolean deleteReplace;
+
     public static MasterBindingIdentity create(Binding binding, PrefixGroup prefixGroup, Source source) {
         return new MasterBindingIdentity(binding, prefixGroup, source);
     }
 
     public static List<MasterBindingIdentity> create(MasterDatabase database, boolean onlyChanged) {
         List<MasterBindingIdentity> identities = new ArrayList<>();
+        Map<String, Binding> deleted = Maps.newHashMap();
+        Multimap<String, MasterBindingIdentity> deletedReplacementCandidates = HashMultimap.create();
+
         if (database.getSource() != null) {
             for (Source source : database.getSource()) {
                 if (source.getPrefixGroup() != null) {
@@ -38,6 +52,11 @@ public class MasterBindingIdentity {
                                     changed = true;
                                 }
 
+                                if(binding.getAction() == DatabaseAction.Delete) {
+                                    // Fixme string as key
+                                    deleted.put(new String(binding.getKey().getIpPrefix().getValue()), binding);
+                                }
+
                                 if (!onlyChanged || changed) {
                                     identities.add(new MasterBindingIdentity(binding, prefixGroup, source));
                                 }
@@ -46,6 +65,52 @@ public class MasterBindingIdentity {
                     }
                 }
             }
+        }
+
+        if (database.getSource() != null) {
+            for (Source source : database.getSource()) {
+                if (source.getPrefixGroup() != null) {
+                    for (PrefixGroup prefixGroup : source.getPrefixGroup()) {
+                        if (prefixGroup.getBinding() != null) {
+                            for (Binding binding : prefixGroup.getBinding()) {
+                                if(binding.getAction() == DatabaseAction.Delete) {
+                                    continue;
+                                }
+                                // Add delete replacements
+                                final String key = new String(binding.getKey().getIpPrefix().getValue());
+                                if(deleted.containsKey(key)) {
+                                    deletedReplacementCandidates.put(key, new MasterBindingIdentity(binding, prefixGroup, source, true));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        ArrayList<MasterBindingIdentity> candidates;
+        for (String key : deletedReplacementCandidates.keySet()) {
+            candidates = Lists.newArrayList(deletedReplacementCandidates.get(key));
+            Collections.sort(candidates, new Comparator<MasterBindingIdentity>() {
+                @Override
+                public int compare(final MasterBindingIdentity b1, final MasterBindingIdentity b2) {
+                    final Binding o1 = b1.getBinding();
+                    final Binding o2 = b2.getBinding();
+                    final int peerSequenceComparison = -1 * (o1.getPeerSequence().getPeer().size() - o2.getPeerSequence().getPeer().size());
+                    if(peerSequenceComparison == 0) {
+                        try {
+                            return Long.valueOf(TimeConv.toLong(o1.getTimestamp())).compareTo(TimeConv.toLong(o2.getTimestamp()));
+                        // FIXME base exception type thrown
+                        } catch (Exception e) {
+                            throw new IllegalArgumentException("Unable to parse datetime to long", e);
+                        }
+                    }
+                    return peerSequenceComparison;
+                }
+            });
+
+            // Adding the most fittest binding replacement for deleted IP
+            identities.add(candidates.get(0));
         }
         return identities;
     }
@@ -65,6 +130,10 @@ public class MasterBindingIdentity {
     protected Source source;
 
     private MasterBindingIdentity(Binding binding, PrefixGroup prefixGroup, Source source) {
+        this(binding, prefixGroup, source, false);
+    }
+
+    private MasterBindingIdentity(Binding binding, PrefixGroup prefixGroup, Source source, boolean deleteReplace) {
         super();
         this.binding = new BindingBuilder(binding).build();
 
@@ -77,6 +146,14 @@ public class MasterBindingIdentity {
         sourceBuilder.setPrefixGroup(new ArrayList<PrefixGroup>());
         sourceBuilder.getPrefixGroup().add(this.prefixGroup);
         this.source = sourceBuilder.build();
+        this.deleteReplace = deleteReplace;
+    }
+
+    /**
+     * @return true if current identity is a fallback binding for a deleted binding with same ip
+     */
+    public boolean isDeleteReplace() {
+        return deleteReplace;
     }
 
     @Override
