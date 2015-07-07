618a7f107d715d4c0afceb8f6d8df5498040087b
Martin Mihálek <martin.mihalek@pantheon.sk>
2015-07-07 12:48:27 +0200

    UT added Tests for sxp/core/behavior/* and refactored useless code
    Change-Id: I21c5967fb4c8d05efd433a2ccff1e98fbae04852
    Signed-off-by: Martin Mihálek <martin.mihalek@pantheon.sk>

---
+++ sxp-core/src/test/java/org/opendaylight/sxp/core/behavior/ContextTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sxp.core.behavior;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.opendaylight.sxp.core.SxpConnection;
+import org.opendaylight.sxp.core.SxpNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.protocol.rev141002.MessageType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.protocol.rev141002.OpenMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.protocol.rev141002.OpenMessageLegacy;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.protocol.rev141002.Version;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.net.SocketAddress;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class) @PrepareForTest({SxpNode.class, StrategyFactory.class}) public class ContextTest {
+
+        @Rule public ExpectedException exception = ExpectedException.none();
+
+        private static SxpConnection connection;
+        private static SxpNode sxpNode;
+        private static ChannelHandlerContext channelHandlerContext;
+        private static Context context;
+
+        @Before public void init() throws Exception {
+                connection = mock(SxpConnection.class);
+                sxpNode = PowerMockito.mock(SxpNode.class);
+                when(connection.getOwner()).thenReturn(sxpNode);
+                PowerMockito.when(sxpNode.getConnection(any(SocketAddress.class))).thenReturn(connection);
+                channelHandlerContext = mock(ChannelHandlerContext.class);
+                Channel channel = mock(Channel.class);
+                when(channel.localAddress()).thenReturn(mock(SocketAddress.class));
+                when(channel.remoteAddress()).thenReturn(mock(SocketAddress.class));
+                when(channelHandlerContext.channel()).thenReturn(channel);
+
+                PowerMockito.mockStatic(StrategyFactory.class);
+                PowerMockito.when(StrategyFactory.getStrategy(any(Context.class), any(Version.class)))
+                        .thenReturn(mock(Strategy.class));
+        }
+
+        @Test public void testExecuteInputMessageStrategy() throws Exception {
+                context = new Context(sxpNode, Version.Version3);
+                Context context_ = new Context(sxpNode, Version.Version4);
+                when(connection.getContext()).thenReturn(context_);
+
+                OpenMessage message = mock(OpenMessage.class);
+                when(message.getVersion()).thenReturn(Version.Version4);
+                when(message.getType()).thenReturn(MessageType.Open);
+                when(connection.getVersion()).thenReturn(Version.Version3);
+
+                exception.expect(IllegalStateException.class);
+                context.executeInputMessageStrategy(channelHandlerContext, connection, message);
+
+                when(message.getType()).thenReturn(MessageType.OpenResp);
+                exception.expect(IllegalStateException.class);
+                context.executeInputMessageStrategy(channelHandlerContext, connection, message);
+        }
+
+        @Test public void testExecuteInputMessageStrategyLegacy() throws Exception {
+                context = new Context(sxpNode, Version.Version4);
+                Context context_ = new Context(sxpNode, Version.Version1);
+                when(connection.getContext()).thenReturn(context_);
+
+                OpenMessageLegacy message = mock(OpenMessageLegacy.class);
+                when(message.getVersion()).thenReturn(Version.Version1);
+                when(message.getType()).thenReturn(MessageType.Open);
+                when(connection.getVersion()).thenReturn(Version.Version1);
+
+                context.executeInputMessageStrategy(channelHandlerContext, connection, message);
+                verify(connection).closeChannelHandlerContext(any(ChannelHandlerContext.class));
+                verify(sxpNode).openConnection(any(SxpConnection.class));
+
+                when(connection.getVersion()).thenReturn(Version.Version4, Version.Version1);
+                context.executeInputMessageStrategy(channelHandlerContext, connection, message);
+                verify(connection).setBehaviorContexts(Version.Version1);
+
+                when(message.getType()).thenReturn(MessageType.OpenResp);
+                context.executeInputMessageStrategy(channelHandlerContext, connection, message);
+                verify(connection, times(2)).setBehaviorContexts(Version.Version1);
+        }
+}
---
+++ sxp-core/src/test/java/org/opendaylight/sxp/core/behavior/Sxpv1Test.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sxp.core.behavior;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.opendaylight.sxp.core.SxpConnection;
+import org.opendaylight.sxp.core.SxpNode;
+import org.opendaylight.sxp.core.messaging.legacy.LegacyMessageFactory;
+import org.opendaylight.sxp.util.exception.ErrorMessageReceivedException;
+import org.opendaylight.sxp.util.exception.message.ErrorMessageException;
+import org.opendaylight.sxp.util.exception.message.UpdateMessageConnectionStateException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.protocol.rev141002.*;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class) @PrepareForTest({SxpNode.class, Context.class}) public class Sxpv1Test {
+
+        @Rule public ExpectedException exception = ExpectedException.none();
+
+        private static Sxpv1 sxpv1;
+        private static ChannelHandlerContext channelHandlerContext;
+        private static SxpConnection connection;
+        private static SxpNode sxpNode;
+
+        @Before public void init() {
+                sxpv1 = new Sxpv1(PowerMockito.mock(Context.class));
+                channelHandlerContext = mock(ChannelHandlerContext.class);
+                connection = mock(SxpConnection.class);
+                when(connection.getVersion()).thenReturn(Version.Version1);
+                when(connection.getMode()).thenReturn(ConnectionMode.None);
+                PowerMockito.mockStatic(LegacyMessageFactory.class);
+                sxpNode = PowerMockito.mock(SxpNode.class);
+                Context context = PowerMockito.mock(Context.class);
+                PowerMockito.when(context.getOwner()).thenReturn(sxpNode);
+                when(connection.getContext()).thenReturn(context);
+        }
+
+        @Test public void testOnChannelActivation() throws Exception {
+                byte i = 1;
+                for (ConnectionMode mode : ConnectionMode.values()) {
+                        when(connection.getMode()).thenReturn(mode);
+
+                        sxpv1.onChannelActivation(channelHandlerContext, connection);
+                        verify(channelHandlerContext, times(i)).writeAndFlush(any(ByteBuf.class));
+                        verify(connection, times(i++)).setStatePendingOn();
+                }
+
+                when(connection.isStateDeleteHoldDown()).thenReturn(true);
+                sxpv1.onChannelActivation(channelHandlerContext, connection);
+                verify(channelHandlerContext, times(i)).writeAndFlush(any(ByteBuf.class));
+                verify(connection, times(i++)).setStatePendingOn();
+                verify(connection).setReconciliationTimer();
+        }
+
+        @Test public void testOnChannelInactivation() throws Exception {
+                when(connection.isStateOn(Matchers.<SxpConnection.ChannelHandlerContextType>any())).thenReturn(true);
+                when(connection.getContextType(any(ChannelHandlerContext.class))).thenReturn(
+                        SxpConnection.ChannelHandlerContextType.ListenerContext);
+
+                when(connection.isPurgeAllMessageReceived()).thenReturn(false);
+                sxpv1.onChannelInactivation(channelHandlerContext, connection);
+                verify(connection).setDeleteHoldDownTimer();
+
+                when(connection.isPurgeAllMessageReceived()).thenReturn(true);
+                sxpv1.onChannelInactivation(channelHandlerContext, connection);
+                verify(connection).setStateOff(any(ChannelHandlerContext.class));
+
+                when(connection.getContextType(any(ChannelHandlerContext.class))).thenReturn(
+                        SxpConnection.ChannelHandlerContextType.SpeakerContext);
+                sxpv1.onChannelInactivation(channelHandlerContext, connection);
+                verify(channelHandlerContext).writeAndFlush(any(ByteBuf.class));
+                verify(connection, times(2)).setStateOff(any(ChannelHandlerContext.class));
+
+        }
+
+        @Test public void testOnInputMessageOpen() throws Exception {
+                OpenMessageLegacy message = mock(OpenMessageLegacy.class);
+                when(message.getVersion()).thenReturn(Version.Version1);
+                when(message.getType()).thenReturn(MessageType.Open);
+                when(message.getPayload()).thenReturn(new byte[] {});
+
+                when(message.getSxpMode()).thenReturn(ConnectionMode.Listener);
+                sxpv1.onInputMessage(channelHandlerContext, connection, message);
+                verify(connection).setMode(ConnectionMode.Speaker);
+                verify(connection).closeChannelHandlerContextComplements(any(ChannelHandlerContext.class));
+                verify(connection).setStateOn();
+                verify(channelHandlerContext).writeAndFlush(any(getClass()));
+
+                when(message.getSxpMode()).thenReturn(ConnectionMode.Speaker);
+                sxpv1.onInputMessage(channelHandlerContext, connection, message);
+                verify(connection).setMode(ConnectionMode.Listener);
+                verify(connection, times(2)).closeChannelHandlerContextComplements(any(ChannelHandlerContext.class));
+                verify(connection, times(2)).setStateOn();
+                verify(channelHandlerContext, times(2)).writeAndFlush(any(getClass()));
+
+                when(message.getType()).thenReturn(MessageType.OpenResp);
+                sxpv1.onInputMessage(channelHandlerContext, connection, message);
+                verify(connection, times(3)).closeChannelHandlerContextComplements(any(ChannelHandlerContext.class));
+                verify(connection, times(3)).setStateOn();
+
+                when(connection.getVersion()).thenReturn(Version.Version4);
+                exception.expect(ErrorMessageException.class);
+                sxpv1.onInputMessage(channelHandlerContext, connection, message);
+        }
+
+        @Test public void testOnInputMessageUpdate() throws Exception {
+                UpdateMessageLegacy messageLegacy = mock(UpdateMessageLegacy.class);
+                when(messageLegacy.getType()).thenReturn(MessageType.Update);
+                when(messageLegacy.getPayload()).thenReturn(new byte[] {});
+
+                when(connection.isStateOn(SxpConnection.ChannelHandlerContextType.ListenerContext)).thenReturn(true);
+                sxpv1.onInputMessage(channelHandlerContext, connection, messageLegacy);
+                verify(connection).setUpdateOrKeepaliveMessageTimestamp();
+                verify(sxpNode).processUpdateMessage(any(UpdateMessageLegacy.class), any(SxpConnection.class));
+
+                when(connection.getState()).thenReturn(ConnectionState.Off);
+                when(connection.isStateOn(SxpConnection.ChannelHandlerContextType.ListenerContext)).thenReturn(false);
+                exception.expect(UpdateMessageConnectionStateException.class);
+                sxpv1.onInputMessage(channelHandlerContext, connection, messageLegacy);
+        }
+
+        @Test public void testOnInputMessageError() throws Exception {
+                ErrorMessage message = mock(ErrorMessage.class);
+                when(message.getInformation()).thenReturn("");
+                when(message.getType()).thenReturn(MessageType.Error);
+                when(message.getPayload()).thenReturn(new byte[] {});
+
+                exception.expect(ErrorMessageReceivedException.class);
+                sxpv1.onInputMessage(channelHandlerContext, connection, message);
+        }
+
+        @Test public void testOnInputMessagePurgeAll() throws Exception {
+                PurgeAllMessage message = mock(PurgeAllMessage.class);
+                when(message.getType()).thenReturn(MessageType.PurgeAll);
+
+                when(connection.getDestination()).thenReturn(
+                        new InetSocketAddress(InetAddress.getByName("0.0.0.0"), 5));
+                sxpv1.onInputMessage(channelHandlerContext, connection, message);
+                verify(connection).setPurgeAllMessageReceived();
+                verify(sxpNode).purgeBindings(any(NodeId.class));
+                verify(sxpNode).notifyService();
+        }
+
+}
