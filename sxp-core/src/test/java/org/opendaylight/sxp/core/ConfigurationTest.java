6373dee3933e687f78090ff75b6a1fb0447b4f5e
Martin Mihálek <martin.mihalek@pantheon.sk>
2015-07-15 13:55:31 +0200

    Bug 4000 Added check for connection mode when handling OpenMessageLegacy, UT updated to reflect current change.
    Change-Id: I90e4cb2178c93baddb2a9022e96c68a3df54e4b0
    Signed-off-by: Martin Mihálek <martin.mihalek@pantheon.sk>

---
+++ sxp-core/src/test/java/org/opendaylight/sxp/core/ConfigurationTest.java
@@ -0,0 +1,67 @@
+package org.opendaylight.sxp.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by martinmihalek on 24.7.2015.
+ */
+public class ConfigurationTest {
+
+        @Test public void testGetCapabilities() throws Exception {
+
+        }
+
+        @Test public void testGetConnectionTimersBoth() throws Exception {
+
+        }
+
+        @Test public void testGetConnectionTimersListener() throws Exception {
+
+        }
+
+        @Test public void testGetConnectionTimersSpeaker() throws Exception {
+
+        }
+
+        @Test public void testGetConstants() throws Exception {
+
+        }
+
+        @Test public void testGetNextNodeName() throws Exception {
+
+        }
+
+        @Test public void testGetNodes() throws Exception {
+
+        }
+
+        @Test public void testGetNodeTimers() throws Exception {
+
+        }
+
+        @Test public void testGetRegisteredNode() throws Exception {
+
+        }
+
+        @Test public void testGetRegisteredNodesIds() throws Exception {
+
+        }
+
+        @Test public void testGetTimerDefault() throws Exception {
+
+        }
+
+        @Test public void testInitializeLogger() throws Exception {
+
+        }
+
+        @Test public void testIsNodesRegistered() throws Exception {
+
+        }
+
+        @Test public void testRegister() throws Exception {
+
+        }
+}
---
+++ sxp-core/src/test/java/org/opendaylight/sxp/util/time/TimeConvTest.java
@@ -0,0 +1,19 @@
+package org.opendaylight.sxp.util.time;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by martinmihalek on 23.7.2015.
+ */
+public class TimeConvTest {
+
+        @Test public void testToDt() throws Exception {
+
+        }
+
+        @Test public void testToLong() throws Exception {
+
+        }
+}
