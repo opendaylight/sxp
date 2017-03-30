b07df48091c68613f119844839988ced10c84b60
Martin Mihálek <mamihale@cisco.com>
2017-03-30 14:40:28 +0200

    Add support for legacy Update MSG in SXP dissector
    - Adds support for parsing Update msg for version 1, 2, 3
    - Adds support for parsing TLV attributes
    - Renamed dissector protocol from SXP4 to SXP
    
    Change-Id: Ic911bb255a89cc316f7235b988def705defb5c5e
    Signed-off-by: Martin Mihálek <mamihale@cisco.com>

--- sxp-dissector/sxp4-update-msg.lua
+++ sxp-dissector/sxp4-update-msg.lua
@@ -19,16 +19,26 @@ function process_update_attribute_value(attr_value_raw, attr_type_value, tree)
         tree:add(mask, "IPv4 mask : " .. mask:uint())
         local ip_address = attr_value_raw(1, 4)
         tree:add(mask, "IPv4 address : " .. tostring(ip_address:ipv4()))
+    elseif attr_type_value == 0x01 or attr_type_value == 0x02 then -- (add | delete) IPv4
+        local ip_address = attr_value_raw(0, 4)
+        process_tlv(attr_value_raw, tree, 4)
+        tree:add(ip_address, "IPv4 address : " .. tostring(ip_address:ipv4()))
     elseif attr_type_value == 0x0c or attr_type_value == 0x0e then -- (add | delete) IPv6 prefix
         local mask = attr_value_raw(0, 1)
         tree:add(mask, "IPv6 mask : " .. mask:uint())
         local ip_address = attr_value_raw(1, 16)
         tree:add(mask, "IPv6 address : " .. tostring(ip_address:ipv6()))
+    elseif attr_type_value == 0x03 or attr_type_value == 0x04 then -- (add | delete) IPv6
+        local ip_address = attr_value_raw(0, 16)
+        process_tlv(attr_value_raw, tree, 16)
+        tree:add(ip_address, "IPv6 address : " .. tostring(ip_address:ipv6()))
     elseif attr_type_value == 0x10 then -- peer sequence
         process_peer_sequence(attr_value_raw, tree)
     elseif attr_type_value == 0x11 then -- sgt
         local sgt = attr_value_raw(0, 2)
         tree:add(sgt, "sgt : " .. sgt:uint())
+    else
+        tree:add(attr_type_value, "unknown: " .. attr_value_raw)
     end
 end
 
@@ -39,5 +49,28 @@ function process_peer_sequence(peerBuffer, tree)
         local peer = peerBuffer(offset, 4)
         tree:add(peer, "peer: 0x" .. tostring(peer))
         offset = offset + 4
+    end
+end
+
+
+function process_tlv(data, tree, address_offset)
+    local offset = address_offset
+    local implicit_tlv_prefix_length = true;
+    while offset < data:len() do
+        local type = data(offset, 4):uint()
+        offset = offset + 4
+        local len = data(offset, 4):uint()
+        offset = offset + 4
+        local value = data(offset, len)
+        if type == 1 then -- sgt
+            tree:add(value, "sgt : " .. value:uint())
+        elseif type == 2 then -- prefix length
+            tree:add(value, "IPv" .. (address_offset == 4 and "4" or "6") .. " mask : " .. value:uint())
+            implicit_tlv_prefix_length = false
+        end
+        offset = offset + len
+    end
+    if implicit_tlv_prefix_length then -- prefix length
+        tree:add("IPv" .. (address_offset == 4 and "4" or "6") .. " mask : " .. (address_offset == 4 and "32" or "128"))
     end
 end
