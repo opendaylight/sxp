2a62bfa6af800d36e2be024c4d8838d8a14cd042
Martin Dindoffer <martin.dindoffer@pantheon.tech>
2017-10-27 10:31:09 +0200

    Add binding deserializer and refactor tests
    Change-Id: Ic20409edc52140545a4e9327043a4cada1acb665
    Signed-off-by: Martin Dindoffer <martin.dindoffer@pantheon.tech>Signed-off-by: Martin Dindoffer <martin.dindoffer@pantheon.tech>

--- sxp-system-tests/sxp-jst/src/test/java/org/opendaylight/sxp/jst/SystemTests.java
+++ sxp-system-tests/sxp-jst/src/test/java/org/opendaylight/sxp/jst/SystemTests.java
@@ -1,17 +1,11 @@
 package org.opendaylight.sxp.jst;
 
-import com.owlike.genson.Genson;
+import org.opendaylight.sxp.jst.client.SxpBindingRestconfClient;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Map;
-import javax.ws.rs.core.MediaType;
+import java.util.concurrent.TimeoutException;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import org.opendaylight.sxp.restconfclient.DSType;
-import org.opendaylight.sxp.restconfclient.RestconfClient;
-import org.opendaylight.sxp.restconfclient.UserCredentials;
 import org.testng.annotations.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,91 +21,46 @@ import org.testng.reporters.Files;
 public class SystemTests {
 
     private static final Logger LOG = LoggerFactory.getLogger(SystemTests.class);
-    private static final String SXP_TOPO_URL = "network-topology:network-topology/topology/sxp";
-    private RestconfClient configClient, operationalClient;
-    private Genson genson;
 
-    @Test
-    public void testNodeAddDeleteRaceCondition() throws IOException, InterruptedException {
-        InputStream node1 = this.getClass().getResourceAsStream("/SXP-node-1.1.5.1.xml");
-        String node1String = Files.readFile(node1);
-        LOG.info("POSTing node 1.1.5.1");
-        Response postResp = configClient.post(node1String, SXP_TOPO_URL);
-        LOG.info("RESP: {}", postResp.getStatusInfo());
-        LOG.info("RESP content: {}", postResp.readEntity(String.class));
-        String nodeId = "1.1.5.1";
-        waitForGetStatus(nodeId, Status.OK);
-        LOG.info("Node found");
-
-        LOG.info("Deleting node {}", nodeId);
-        Response deleteResp = configClient.delete(SXP_TOPO_URL, "node", nodeId);
-        LOG.info("RESP: {}", deleteResp.getStatusInfo());
-
-        Response post2Resp = configClient.post(node1String, SXP_TOPO_URL);
-        LOG.info("RESP: {}", post2Resp.getStatusInfo());
-        LOG.info("RESP content: {}", post2Resp.readEntity(String.class));
-    }
-
-    private void waitForGetStatus(String nodeId, Status status) throws InterruptedException {
-        Response nodeResp = operationalClient.get(SXP_TOPO_URL, "node", nodeId);
-        while (nodeResp.getStatus() != status.getStatusCode()) {
-            Thread.sleep(100);
-            nodeResp = operationalClient.get(SXP_TOPO_URL, "node", nodeId);
-        }
-    }
+    private SxpBindingRestconfClient sxpClient;
 
     @BeforeClass
     public void init() throws Exception {
-        genson = new Genson();
-        configClient = new RestconfClient.Builder()
-                .setAcceptType(MediaType.APPLICATION_JSON_TYPE)
-                .setContentType(MediaType.APPLICATION_XML_TYPE)
-                .setCredentials(new UserCredentials("admin", "admin"))
-                .setDsType(DSType.CONFIG)
-                .setHost("localhost")
-                .setPort(8181)
-                .build();
-        operationalClient = new RestconfClient.Builder()
-                .setAcceptType(MediaType.APPLICATION_JSON_TYPE)
-                .setContentType(MediaType.APPLICATION_XML_TYPE)
-                .setCredentials(new UserCredentials("admin", "admin"))
-                .setDsType(DSType.OPERATIONAL)
-                .setHost("localhost")
-                .setPort(8181)
-                .build();
-    }
-
-    @AfterTest
-    public void cleanupTopo() {
-        LOG.info("Cleaning SXP topo...");
-        Response deleteResp = configClient.delete(SXP_TOPO_URL);
-        LOG.info("Delete status: {}", deleteResp.getStatusInfo());
-        waitForEmptyTopo();
-    }
-
-    public void waitForEmptyTopo() {
-        Map<String, Object> topology = getTopo();
-        while (!isTopoEmpty(topology)) {
-            topology = getTopo();
-        }
-    }
-
-    private Map<String, Object> getTopo() {
-        Response getResp = operationalClient.get(SXP_TOPO_URL);
-        String topoString = getResp.readEntity(String.class);
-        Map<String, Object> topology = genson.deserialize(topoString, Map.class);
-        return topology;
-    }
-
-    public boolean isTopoEmpty(Map<String, Object> topo) {
-        List<Map> topos = (List) topo.get("topology");
-        return topos.get(0).size() == 1;
+        this.sxpClient = new SxpBindingRestconfClient();
     }
 
     @BeforeMethod
     public void preTestHook(Method method) {
         String testName = method.getName();
         LOG.info("Running test {}", testName);
+    }
+
+    @Test
+    public void testNodeAddDeleteRaceCondition() throws IOException, InterruptedException, TimeoutException {
+        InputStream node1 = SystemTests.class.getResourceAsStream("/SXP-node-1.1.5.1.xml");
+        String nodeId = "1.1.5.1";
+        String nodePayload = Files.readFile(node1);
+        LOG.info("POSTing node {}", nodeId);
+        Response postResp = sxpClient.postNode(nodePayload);
+        LOG.info("RESP: {}", postResp.getStatusInfo());
+
+        sxpClient.waitForOperationalNodeAppearance(nodeId, 5_000);
+        LOG.info("Node found");
+
+        LOG.info("Deleting node {}", nodeId);
+        Response deleteNodeResp = sxpClient.deleteNode(nodeId);
+        LOG.info("RESP: {}", deleteNodeResp.getStatusInfo());
+
+        Response post2Resp = sxpClient.postNode(nodePayload);
+        LOG.info("RESP: {}", post2Resp.getStatusInfo());
+    }
+
+    @AfterTest
+    public void cleanupTopo() throws InterruptedException, TimeoutException {
+        LOG.info("Cleaning SXP topo...");
+        Response deleteTopoResp = sxpClient.deleteTopo();
+        LOG.info("Delete status: {}", deleteTopoResp.getStatusInfo());
+        sxpClient.waitForEmptyTopo(5_000);
     }
 
 }
