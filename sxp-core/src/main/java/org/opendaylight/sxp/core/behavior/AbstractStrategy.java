8a29f4efa405947ad46e481aacff460337a5d3ba
Martin Dindoffer <mdindoff@cisco.com>
2018-02-23 17:55:34 +0100

    SXP-135 Shutdown connections on Openmsg when connection is ON
    Change-Id: Ib8e0a3b4fdbdce059cb24d4f2117f5640e657bb7
    Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>

---
+++ sxp-core/src/main/java/org/opendaylight/sxp/core/behavior/AbstractStrategy.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sxp.core.behavior;
+
+import io.netty.channel.ChannelHandlerContext;
+import org.opendaylight.sxp.core.SxpConnection;
+import org.opendaylight.sxp.core.messaging.MessageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractStrategy implements Strategy {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractStrategy.class);
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onChannelInactivation(ChannelHandlerContext ctx, SxpConnection connection) {
+        LOG.info("{} Handling channel inactivation", connection);
+        SxpConnection.ChannelHandlerContextType type = connection.getContextType(ctx);
+        if (connection.isStateOn(type)) {
+            switch (type) {
+                case LISTENER_CNTXT:
+                    connection.setDeleteHoldDownTimer();
+                    return;
+                case SPEAKER_CNTXT:
+                    LOG.trace("Context status on channel inactivation: {}, {}, writing purgeall", ctx.isRemoved(), ctx.channel());
+                    ctx.writeAndFlush(MessageFactory.createPurgeAll());
+                    break;
+            }
+        }
+        connection.setStateOff(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onException(ChannelHandlerContext ctx, SxpConnection connection) {
+        LOG.warn("{} onException", connection);
+    }
+}
