d89853f189e4441ae6ff14de475f0010a1c79ee5
Martin Dindoffer <mdindoff@cisco.com>
2018-03-15 15:30:17 +0100

    SXP-137
    Change-Id: I61a53e9d58a22cd8205838f419f2d67d7d1ae23a
    Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>

---
+++ sxp-core/src/main/java/org/opendaylight/sxp/util/time/connection/RetryOpenTimerTask.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sxp.util.time.connection;
+
+import io.netty.util.concurrent.Future;
+import java.util.concurrent.Callable;
+import org.opendaylight.sxp.core.SxpConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RetryOpenTimerTask implements Callable<Future<Void>> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RetryOpenTimerTask.class);
+
+    private final SxpConnection sxpConnection;
+
+    public RetryOpenTimerTask(SxpConnection sxpConnection) {
+        this.sxpConnection = sxpConnection;
+    }
+
+    @Override
+    public Future<Void> call() {
+        LOG.info("{} Retry Open timer has expired, about to reconnect", sxpConnection);
+        return sxpConnection.openConnection();
+    }
+}
