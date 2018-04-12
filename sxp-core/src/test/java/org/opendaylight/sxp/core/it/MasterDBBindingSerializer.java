9d672f001d4451f3c8e05e4ae6752f32f496eb37
Martin Dindoffer <mdindoff@cisco.com>
2018-04-12 16:54:48 +0200

    Hajzelcast
    Change-Id: I858d6bd37d37007b1cf970500f255a4fb4c0d3bb
    Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>

---
+++ sxp-core/src/test/java/org/opendaylight/sxp/core/it/MasterDBBindingSerializer.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sxp.core.it;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.StreamSerializer;
+import java.io.IOException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.DateAndTime;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.Sgt;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.master.database.fields.MasterDatabaseBinding;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.master.database.fields.MasterDatabaseBindingBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.master.database.fields.MasterDatabaseBindingKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.peer.sequence.fields.PeerSequence;
+
+public class MasterDBBindingSerializer implements StreamSerializer<MasterDatabaseBinding> {
+
+    @Override
+    public void write(ObjectDataOutput out, MasterDatabaseBinding object) throws IOException {
+        out.writeObject(object.getIpPrefix());
+        out.writeObject(object.getKey());
+        out.writeObject(object.getPeerSequence());
+        out.writeObject(object.getSecurityGroupTag());
+        out.writeObject(object.getTimestamp());
+    }
+
+    @Override
+    public MasterDatabaseBinding read(ObjectDataInput in) throws IOException {
+        IpPrefix ipPrefix = in.readObject();
+        MasterDatabaseBindingKey key = in.readObject();
+        PeerSequence ps = in.readObject();
+        Sgt binding = in.readObject();
+        DateAndTime date = in.readObject();
+        return new MasterDatabaseBindingBuilder().setIpPrefix(ipPrefix)
+                .setKey(key)
+                .setPeerSequence(ps)
+                .setSecurityGroupTag(binding)
+                .setTimestamp(date)
+                .build();
+    }
+
+    @Override
+    public int getTypeId() {
+        return 1;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
