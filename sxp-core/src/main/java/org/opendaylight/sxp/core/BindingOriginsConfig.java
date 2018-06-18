c21dbfd90bdc2c171a289a3f5474a7992c560162
Martin Dindoffer <mdindoff@cisco.com>
2018-06-18 13:08:03 +0200

    Create add, update and delete binding origins RPCs
    Change-Id: I1994252d7c09f4075f7202e25ca668868092c1c3
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- sxp-core/src/main/java/org/opendaylight/sxp/core/BindingOriginsConfig.java
+++ sxp-core/src/main/java/org/opendaylight/sxp/core/BindingOriginsConfig.java
@@ -24,6 +24,15 @@ public enum BindingOriginsConfig {
     public static final OriginType LOCAL_ORIGIN = OriginType.getDefaultInstance("LOCAL");
     public static final OriginType NETWORK_ORIGIN = OriginType.getDefaultInstance("NETWORK");
     public static final Map<OriginType, Short> DEFAULT_ORIGIN_PRIORITIES;
+
+    public static final String BINDING_ORIGIN_ALREADY_EXIST = "Binding origin: {} already exist";
+    public static final String PRIORITY_WANTED_TO_BE_USED_IS_ALREADY_USED = "Priority wanted to be used: {} is already used";
+    public static final String BINDING_ORIGIN_TO_BE_UPDATED_NOT_FOUND = "Binding origin to be updated: {} not found";
+    public static final String BINDING_ORIGIN_DEFAULT_VALUE_CANNOT_BE_DELETED = "Binding origin default value: {} cannot be deleted";
+    public static final String BINDING_ORIGIN_TO_BE_DELETED_NOT_FOUND = "Binding origin to be deleted: {} not found";
+    public static final String PROVIDED_ORIGIN_TYPES_DO_NOT_CONTAIN_THE_REQUIRED_DEFAULTS = "Provided origin types do not contain the required defaults.";
+    public static final String PROVIDED_ORIGIN_TYPES_HAVE_CONFLICTING_PRIORITIES = "Provided origin types have conflicting priorities.";
+
     static {
         Map<OriginType, Short> defaultPrios = new HashMap<>();
         defaultPrios.put(LOCAL_ORIGIN, (short) 1);
@@ -34,15 +43,13 @@ public enum BindingOriginsConfig {
     private static final Logger LOG = LoggerFactory.getLogger(BindingOriginsConfig.class.getName());
     private final Map<OriginType, Short> bindingOrigins = Collections.synchronizedMap(new HashMap<>());
 
-    public Map<OriginType, Short> getBindingOrigins() {
-        return bindingOrigins;
-    }
-
     public boolean addBindingOrigin(OriginType origin, Short priority) {
         if (bindingOrigins.containsKey(origin)) {
+            LOG.warn(BINDING_ORIGIN_ALREADY_EXIST, origin.getValue());
             return false;
         }
         if (bindingOrigins.containsValue(priority)) {
+            LOG.warn(PRIORITY_WANTED_TO_BE_USED_IS_ALREADY_USED, priority);
             return false;
         }
 
@@ -54,18 +61,44 @@ public enum BindingOriginsConfig {
         origins.forEach(bindingOrigin -> addBindingOrigin(bindingOrigin.getOrigin(), bindingOrigin.getPriority()));
     }
 
+    public boolean updateBindingOrigin(OriginType origin, Short priority) {
+        if (!bindingOrigins.containsKey(origin)) {
+            LOG.warn(BINDING_ORIGIN_TO_BE_UPDATED_NOT_FOUND, origin.getValue());
+            return false;
+        }
+        if (bindingOrigins.containsValue(priority)) {
+            LOG.warn(PRIORITY_WANTED_TO_BE_USED_IS_ALREADY_USED, priority);
+            return false;
+        }
+
+        bindingOrigins.put(origin, priority);
+        return true;
+    }
+
+    public boolean deleteBindingOrigin(OriginType origin) {
+        if (LOCAL_ORIGIN.equals(origin) || NETWORK_ORIGIN.equals(origin)) {
+            LOG.warn(BINDING_ORIGIN_DEFAULT_VALUE_CANNOT_BE_DELETED, origin.getValue());
+            return false;
+        }
+        if (!bindingOrigins.containsKey(origin)) {
+            LOG.warn(BINDING_ORIGIN_TO_BE_DELETED_NOT_FOUND, origin.getValue());
+            return false;
+        }
+
+        bindingOrigins.remove(origin);
+        return true;
+    }
+
     public void validateOriginBindings(List<BindingOrigin> origins) {
         final Set<OriginType> types = origins.stream().map(BindingOrigin::getOrigin).collect(Collectors.toSet());
         if (!types.contains(LOCAL_ORIGIN) || !types.contains(NETWORK_ORIGIN)) {
-            final String msg = "Provided origin types do not contain the required defaults.";
-            LOG.error(msg);
-            throw new IllegalStateException(msg);
+            LOG.error(PROVIDED_ORIGIN_TYPES_DO_NOT_CONTAIN_THE_REQUIRED_DEFAULTS);
+            throw new IllegalStateException(PROVIDED_ORIGIN_TYPES_DO_NOT_CONTAIN_THE_REQUIRED_DEFAULTS);
         }
         // check if some priority value is not used for more origins
         if (types.size() != origins.size()) {
-            final String msg = "Provided origin types have conflicting priorities.";
-            LOG.error(msg);
-            throw new IllegalStateException(msg);
+            LOG.error(PROVIDED_ORIGIN_TYPES_HAVE_CONFLICTING_PRIORITIES);
+            throw new IllegalStateException(PROVIDED_ORIGIN_TYPES_HAVE_CONFLICTING_PRIORITIES);
         }
     }
 }
