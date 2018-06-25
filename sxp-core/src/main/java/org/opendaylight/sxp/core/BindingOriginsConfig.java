c8995377568fa6813dc67e3be78d6d48e1096cdc
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2018-06-25 15:04:23 +0200

    Move binding origins configuration to singleton
    Change-Id: I438ed5e359726353c9020d2306e78d459fa99e15
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

---
+++ sxp-core/src/main/java/org/opendaylight/sxp/core/BindingOriginsConfig.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sxp.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.config.rev180611.OriginType;
+
+public enum BindingOriginsConfig {
+    INSTANCE;
+
+    public static final OriginType LOCAL_ORIGIN = OriginType.getDefaultInstance("LOCAL");
+    public static final OriginType NETWORK_ORIGIN = OriginType.getDefaultInstance("NETWORK");
+
+    private final Map<OriginType, Short> bindingOrigins = new HashMap<>();
+}
