bd9fe4e59942537f1893f0209172675ed3c2bc89
Ivan Hrasko <ihrasko@cisco.com>
2018-07-02 15:26:07 +0200

    Add priority checks to in-memory DBs
    - all in-memory bindings are stored only
    in one map
    - when adding higher priority binding for
    the same ip prefix then previous binding with
    lower priority is replaced
    - when adding multiple bindings for the
    same ip prefix at once then only the binding
    with the highest priority is saved to database
    
    Change-Id: I4172283ffd78200d2dbf7dbfe6d500556ad78933
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>
    Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>

--- sxp-core/src/main/java/org/opendaylight/sxp/util/database/MasterDBBindingComparator.java
+++ sxp-core/src/main/java/org/opendaylight/sxp/util/database/MasterDBBindingComparator.java
@@ -7,12 +7,13 @@
  */
 package org.opendaylight.sxp.util.database;
 
-import static org.opendaylight.sxp.core.BindingOriginsConfig.LOCAL_ORIGIN;
-import static org.opendaylight.sxp.core.BindingOriginsConfig.NETWORK_ORIGIN;
 import static org.opendaylight.sxp.util.database.MasterDatabase.getPeerSequenceLength;
 
 import java.util.Comparator;
+import java.util.Map;
+import org.opendaylight.sxp.core.BindingOriginsConfig;
 import org.opendaylight.sxp.util.time.TimeConv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.config.rev180611.OriginType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.master.database.fields.MasterDatabaseBinding;
 
 public enum MasterDBBindingComparator implements Comparator<MasterDatabaseBinding> {
@@ -21,12 +22,26 @@ public enum MasterDBBindingComparator implements Comparator<MasterDatabaseBindin
 
     @Override
     public int compare(MasterDatabaseBinding mdb1, MasterDatabaseBinding mdb2) {
-        if ((mdb1.getOrigin() == LOCAL_ORIGIN) && (mdb2.getOrigin() == NETWORK_ORIGIN)) {
+        // binding priority
+        final Map<OriginType, Integer> bindingOrigins = BindingOriginsConfig.INSTANCE.getBindingOrigins();
+        final Integer o1Priority = bindingOrigins.get(mdb1.getOrigin());
+        final Integer o2Priority = bindingOrigins.get(mdb2.getOrigin());
+
+        if (o1Priority == null) {
+            throw new IllegalArgumentException("Cannot find binding priority: " + mdb1.getOrigin().getValue());
+        }
+        if (o2Priority == null) {
+            throw new IllegalArgumentException("Cannot find binding priority: " + mdb2.getOrigin().getValue());
+        }
+
+        if (o1Priority < o2Priority) {
             return -1;
         }
-        if ((mdb1.getOrigin() == NETWORK_ORIGIN) && (mdb2.getOrigin() == LOCAL_ORIGIN)) {
+        if (o1Priority > o2Priority) {
             return 1;
         }
+
+        // peer sequence length
         int o1PSLength = getPeerSequenceLength(mdb1);
         int o2PSLength = getPeerSequenceLength(mdb2);
         if (o1PSLength < o2PSLength) {
@@ -35,9 +50,12 @@ public enum MasterDBBindingComparator implements Comparator<MasterDatabaseBindin
         if (o1PSLength > o2PSLength) {
             return 1;
         }
+
+        // creation time
         if (TimeConv.toLong(mdb1.getTimestamp()) > TimeConv.toLong(mdb2.getTimestamp())) {
             return -1;
         }
+
         return 0;
     }
 }
