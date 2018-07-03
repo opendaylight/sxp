6e63e9a97354aaae4d4cc54612148abef416b75b
Ivan Hrasko <ihrasko@cisco.com>
2018-07-03 15:52:08 +0200

    Remove methods used solely for local bindings
    - removed addLocalBindings(), deleteBindingsLocal() and getLocalBindings()
    from MasterDatabaseInf
    - this change is needed after configurable origin was introduced
    into bindings
    
    Change-Id: If1dfc27791f9b91daf77d4806fb29a3b2a97d34d
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- sxp-core/src/main/java/org/opendaylight/sxp/util/database/spi/MasterDatabaseInf.java
+++ sxp-core/src/main/java/org/opendaylight/sxp/util/database/spi/MasterDatabaseInf.java
@@ -8,7 +8,6 @@
 
 package org.opendaylight.sxp.util.database.spi;
 
-import java.util.Collection;
 import java.util.List;
 import org.opendaylight.sxp.core.SxpDomain;
 import org.opendaylight.sxp.core.service.BindingDispatcher;
@@ -32,29 +31,6 @@ public interface MasterDatabaseInf extends AutoCloseable {
      * @return All bindings stored in MasterDatabase
      */
     List<MasterDatabaseBinding> getBindings();
-
-    /**
-     * @return Local bindings stored in MasterDatabase
-     */
-    Collection<MasterDatabaseBinding> getLocalBindings();
-
-    /**
-     * Adds bindings into MasterDatabase as local bindings
-     *
-     * @param bindings List of bindings that will be added
-     * @param <T>      Any type extending SxpBindingsFields
-     * @return List of Bindings that were actually added
-     */
-    <T extends SxpBindingFields> List<MasterDatabaseBinding> addLocalBindings(List<T> bindings);
-
-    /**
-     * Removes bindings from MasterDatabase that were stored as local
-     *
-     * @param bindings List of bindings that will be removed
-     * @param <T>      Any   type extending SxpBindingsFields
-     * @return List of bindings that were actually removed
-     */
-    <T extends SxpBindingFields> List<MasterDatabaseBinding> deleteBindingsLocal(List<T> bindings);
 
     /**
      * Adds bindings into MasterDatabase as learned bindings
