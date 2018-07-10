c41ad9b60df572886f05fca04e0c9624d87878af
Martin Dindoffer <mdindoff@cisco.com>
2018-07-10 12:53:27 +0200

    Respect origin priorities in Hazelcast master DB
    Change-Id: I7368a1143f06795a30c7efbce8b2b7d5d6d4a6db
    Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>

--- sxp-core/src/test/java/org/opendaylight/sxp/core/it/HazelcastBackedMasterDBIT.java
+++ sxp-core/src/test/java/org/opendaylight/sxp/core/it/HazelcastBackedMasterDBIT.java
@@ -14,9 +14,11 @@ import static org.junit.Assert.assertTrue;
 import com.hazelcast.config.Config;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -28,6 +30,7 @@ import org.opendaylight.sxp.core.SxpNode;
 import org.opendaylight.sxp.util.database.HazelcastBackedMasterDB;
 import org.opendaylight.sxp.util.time.TimeConv;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.config.rev180611.OriginType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.Sgt;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.SxpBindingFields;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.database.rev160308.master.database.fields.MasterDatabaseBindingBuilder;
@@ -70,11 +73,19 @@ public class HazelcastBackedMasterDBIT {
     }
 
     private <T extends SxpBindingFields> T getBinding(String prefix, int sgt, String... peers) {
+        return getBinding(prefix, sgt, BindingOriginsConfig.LOCAL_ORIGIN, peers);
+    }
+
+    private <T extends SxpBindingFields> T getNetworkBinding(String prefix, int sgt, String... peers) {
+        return getBinding(prefix, sgt, BindingOriginsConfig.NETWORK_ORIGIN, peers);
+    }
+
+    private <T extends SxpBindingFields> T getBinding(String prefix, int sgt, OriginType origin, String... peers) {
         MasterDatabaseBindingBuilder bindingBuilder = new MasterDatabaseBindingBuilder();
         bindingBuilder.setIpPrefix(new IpPrefix(prefix.toCharArray()));
         bindingBuilder.setSecurityGroupTag(new Sgt(sgt));
         bindingBuilder.setTimestamp(TimeConv.toDt(time += 1000));
-        bindingBuilder.setOrigin(BindingOriginsConfig.LOCAL_ORIGIN);
+        bindingBuilder.setOrigin(origin);
         PeerSequenceBuilder sequenceBuilder = new PeerSequenceBuilder();
         sequenceBuilder.setPeer(new ArrayList<>());
         for (int i = 0; i < peers.length; i++) {
@@ -91,10 +102,10 @@ public class HazelcastBackedMasterDBIT {
 
     private <T extends SxpBindingFields, R extends SxpBindingFields> void assertBindings(List<T> bindings1,
                                                                                          List<R> bindings2) {
-        bindings1.stream()
-                .forEach(b -> assertTrue(bindings2.stream()
+        bindings1.forEach(b -> assertTrue(bindings2.stream()
                         .anyMatch(r -> r.getSecurityGroupTag().getValue().equals(b.getSecurityGroupTag().getValue())
-                                && Arrays.equals(r.getIpPrefix().getValue(), b.getIpPrefix().getValue()))));
+                                && Arrays.equals(r.getIpPrefix().getValue(), b.getIpPrefix().getValue())
+                                && r.getOrigin().equals(b.getOrigin()))));
     }
 
     @Test
@@ -153,6 +164,84 @@ public class HazelcastBackedMasterDBIT {
                 mergeBindings(getBinding("15.15.15.15/24", 15, "0.10.10.10"),
                         getBinding("2.2.2.20/32", 2000, "200.200.200.200")));
         assertEquals(0, database.getBindings().size());
+    }
+
+    @Test
+    public void testAddLowerPriorityBinding() {
+        final String prefix = "1.1.1.1/32";
+        final int sgt = 20;
+        final String peer = "10.10.10.10";
+
+        // add local binding
+        final SxpBindingFields localBinding = getBinding(prefix, sgt, peer);
+        assertBindings(database.addBindings(Collections.singletonList(localBinding)),
+                Collections.singletonList(localBinding));
+        assertBindings(database.getBindings(), Collections.singletonList(localBinding));
+
+        // add binding of lower priority - lower priority binding is not added
+        final SxpBindingFields networkBinding = getNetworkBinding(prefix, sgt, peer);
+        assertTrue(database.addBindings(Collections.singletonList(networkBinding)).isEmpty());
+
+        // the previous binding is still in database
+        assertBindings(database.getBindings(), Collections.singletonList(localBinding));
+    }
+
+    @Test
+    public void testAddHigherPriorityBinding() {
+        final String prefix = "1.1.1.1/32";
+        final int sgt = 20;
+        final String peer = "10.10.10.10";
+
+        // add network binding
+        final SxpBindingFields networkBinding = getNetworkBinding(prefix, sgt, peer);
+        assertBindings(database.addBindings(Collections.singletonList(networkBinding)),
+                Collections.singletonList(networkBinding));
+        assertBindings(database.getBindings(), Collections.singletonList(networkBinding));
+
+        // add binding of higher priority - previous lower priority binding is replaced
+        final SxpBindingFields localBinding = getBinding(prefix, sgt, peer);
+        assertBindings(database.addBindings(Collections.singletonList(localBinding)),
+                Collections.singletonList(localBinding));
+
+        // the new binding replaced previous one
+        assertBindings(database.getBindings(), Collections.singletonList(localBinding));
+    }
+
+    @Test
+    public void testAddBindingsWithDifferentPriorities() {
+        // add two bindings with different priorities for the same ip prefix at once
+        final String prefix = "1.1.1.1/32";
+        final int sgt = 20;
+        final SxpBindingFields networkBinding = getNetworkBinding(prefix, sgt);
+        final SxpBindingFields localBinding = getBinding(prefix, sgt);
+
+        // only the binding with higher priority should be added
+        assertEquals(1, database.addBindings(mergeBindings(networkBinding, localBinding)).size());
+        assertBindings(database.getBindings(), Collections.singletonList(localBinding));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddBindingsNotDefinedPriority() {
+        final String cluster = "CLUSTER";
+        final SxpBindingFields binding = getBinding("1.1.1.1/32", 10, new OriginType(cluster));
+        database.addBindings(Collections.singletonList(binding));
+    }
+
+    @Test
+    public void testGetBindings() {
+        final SxpBindingFields localBinding = getBinding("1.1.1.1/32", 10);
+        final SxpBindingFields networkBinding = getNetworkBinding("2.2.2.2/32", 20);
+        assertBindings(database.addBindings(mergeBindings(localBinding, networkBinding)),
+                mergeBindings(localBinding, networkBinding));
+
+        // get all bindings
+        assertBindings(database.getBindings(), mergeBindings(localBinding, networkBinding));
+        // get local bindings
+        assertBindings(database.getBindings(BindingOriginsConfig.LOCAL_ORIGIN),
+                Collections.singletonList(localBinding));
+        // get network bindings
+        assertBindings(database.getBindings(BindingOriginsConfig.NETWORK_ORIGIN),
+                Collections.singletonList(networkBinding));
     }
 
 }
