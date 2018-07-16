88066d4a13758f972f5a636d96ce59e8c7485cb6
Martin Dindoffer <mdindoff@cisco.com>
2018-07-16 14:47:53 +0200

    Fix simultaneous connection problems and add ITs for Binding Origins
    This commit fixes long standing bugs, where two nodes end up in
    a corrupted state if they happen to connect to each other at the same time.
    Fix is basically to just close the correct contexts and only when there is no
    handshake in progress.
    Also fixes the incorrect addition of the GLOBAL domain when there is one
    provided during the construction of an SXP node.
    Finally, it adds some IT for checking correct binding origin priorities
    handling.
    
    Change-Id: I29857bfc297c16cbe0b41135b7edbdc2c03641b5
    Signed-off-by: Martin Dindoffer <mdindoff@cisco.com>

---
+++ sxp-core/src/main/java/org/opendaylight/sxp/util/netty/InetAddressExtractor.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sxp.util.netty;
+
+import com.google.common.net.InetAddresses;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+public class InetAddressExtractor {
+
+    private InetAddressExtractor() {
+    }
+
+    public static InetAddress getRemoteInetAddressFrom(ChannelHandlerContext ctx) {
+        Channel channel = ctx.channel();
+        if (channel != null) {
+            String remoteAddrString = ((InetSocketAddress) channel.remoteAddress()).getAddress().getHostAddress();
+            return InetAddresses.forString(remoteAddrString);
+        } else {
+            return null;
+        }
+    }
+
+    public static InetAddress getLocalInetAddressFrom(ChannelHandlerContext ctx) {
+        Channel channel = ctx.channel();
+        if (channel != null) {
+            String localAddrString = ((InetSocketAddress) ctx.channel().localAddress()).getAddress().getHostAddress();
+            return InetAddresses.forString(localAddrString);
+        } else {
+            return null;
+        }
+    }
+}
