fdf57b2050c32802acbd388c8719ecb744b56be5
Ivan Hrasko <ihrasko@cisco.com>
2018-08-03 10:45:32 +0200

    Optimize imports
    Change-Id: I955b642c2472dacaeab7da707da5f3b3e491554d
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- sxp-core/src/test/java/org/opendaylight/sxp/util/time/TimeConvTest.java
+++ sxp-core/src/test/java/org/opendaylight/sxp/util/time/TimeConvTest.java
@@ -7,13 +7,13 @@
  */
 package org.opendaylight.sxp.util.time;
 
-import java.lang.reflect.Constructor;
-import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Constructor;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
