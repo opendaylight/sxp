f62721519f6198351e16dc64be7b841337350a96
Ivan Hrasko <ihrasko@cisco.com>
2018-09-11 16:36:18 +0200

    WIP: Neon MRI changes
    This patch bumps:
    - odlparent to 4.0.2
    - yangtools to 2.1.2
    - mdsal to 3.0.1
    
    - Removed Powermock due to incompatibility
    with Mockito 2
    
    - Adapted to mdsal.common.api removal
    and model updates
    
    - Removed mocks of ListenableFuture class
    
    JIRA: SXP-151
    
    Change-Id: Ib84a7badf5c17cdb8171b1c627b300603190826f
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- sxp-core/src/test/java/org/opendaylight/sxp/core/service/SslContextFactoryTest.java
+++ sxp-core/src/test/java/org/opendaylight/sxp/core/service/SslContextFactoryTest.java
@@ -8,50 +8,35 @@
 
 package org.opendaylight.sxp.core.service;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import java.io.File;
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev160308.PathType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev160308.StoreType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev160308.security.fields.Tls;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev160308.security.fields.TlsBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev160308.tls.security.fields.KeystoreBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.node.rev160308.tls.security.fields.TruststoreBuilder;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore({"javax.net.ssl.*","javax.security.*"})
 public class SslContextFactoryTest {
 
-    private SslContextFactory contextFactory;
-    private Tls tls;
+    private static final Tls TLS = new TlsBuilder()
+            .setCertificatePassword("admin123")
+            .setKeystore(new KeystoreBuilder()
+                    .setType(StoreType.JKS)
+                    .setPathType(PathType.CLASSPATH)
+                    .setLocation("keystore")
+                    .setPassword("admin123")
+                    .build())
+            .setTruststore(new TruststoreBuilder()
+                    .setType(StoreType.JKS)
+                    .setPathType(PathType.CLASSPATH)
+                    .setLocation("truststore")
+                    .setPassword("admin123")
+                    .build())
+            .build();
 
-    @Before
-    public void setUp() throws Exception {
-        tls =
-                new TlsBuilder().setCertificatePassword("admin123")
-                        .setKeystore(new KeystoreBuilder().setType(StoreType.JKS)
-                                .setPathType(PathType.CLASSPATH)
-                                .setLocation("keystore")
-                                .setPassword("admin123")
-                                .build())
-                        .setTruststore(new TruststoreBuilder().setType(StoreType.JKS)
-                                .setPathType(PathType.CLASSPATH)
-                                .setLocation("truststore")
-                                .setPassword("admin123")
-                                .build())
-                        .build();
-    }
+    private SslContextFactory contextFactory;
 
     @Test
     public void asInputStream() throws Exception {
@@ -84,7 +69,7 @@ public class SslContextFactoryTest {
         Assert.assertNotNull(contextFactory.getClientContext());
         Assert.assertFalse(contextFactory.getClientContext().isPresent());
 
-        contextFactory = new SslContextFactory(tls);
+        contextFactory = new SslContextFactory(TLS);
         Assert.assertNotNull(contextFactory);
         Assert.assertNotNull(contextFactory.getClientContext());
         Assert.assertTrue(contextFactory.getClientContext().isPresent());
@@ -98,35 +83,10 @@ public class SslContextFactoryTest {
         Assert.assertNotNull(contextFactory.getServerContext());
         Assert.assertFalse(contextFactory.getServerContext().isPresent());
 
-        contextFactory = new SslContextFactory(tls);
+        contextFactory = new SslContextFactory(TLS);
         Assert.assertNotNull(contextFactory);
         Assert.assertNotNull(contextFactory.getServerContext());
         Assert.assertTrue(contextFactory.getServerContext().isPresent());
         Assert.assertTrue(contextFactory.getServerContext().get().isServer());
     }
-
-    @Test
-    public void testConstructorErrorHandling() throws Exception {
-        Tls tlsMock = mock(Tls.class);
-
-        when(tlsMock.getCertificatePassword()).thenThrow(IOException.class);
-        SslContextFactory sslContextFactory = new SslContextFactory(tlsMock);
-        Assert.assertNotNull(sslContextFactory);
-
-        Mockito.reset(tlsMock);
-        when(tlsMock.getCertificatePassword()).thenThrow(NoSuchAlgorithmException.class);
-        sslContextFactory = new SslContextFactory(tlsMock);
-        Assert.assertNotNull(sslContextFactory);
-
-        Mockito.reset(tlsMock);
-        when(tlsMock.getCertificatePassword()).thenThrow(CertificateException.class);
-        sslContextFactory = new SslContextFactory(tlsMock);
-        Assert.assertNotNull(sslContextFactory);
-
-        Mockito.reset(tlsMock);
-        when(tlsMock.getCertificatePassword()).thenThrow(Exception.class);
-        sslContextFactory = new SslContextFactory(tlsMock);
-        Assert.assertNotNull(sslContextFactory);
-    }
-
 }
