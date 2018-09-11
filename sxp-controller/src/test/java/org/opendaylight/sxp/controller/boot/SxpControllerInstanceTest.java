912afc0027567a512b6fe0e2bee500256e09fdc5
Ivan Hrasko <ihrasko@cisco.com>
2018-09-11 16:36:18 +0200

    WIP: Neon MRI changes
    This patch bumps:
    - odlparent to 4.0.2
    - yangtools to 2.1.2
    - mdsal to 3.0.1
    
    - Removed Powermock due to incompatibility
    with Mockito 2
    
    - Adapted to mdsal.common.api removal
    and model updates
    
    - Removed mocks of ListenableFuture class
    
    JIRA: SXP-151
    Change-Id: Ib84a7badf5c17cdb8171b1c627b300603190826f
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- sxp-controller/src/test/java/org/opendaylight/sxp/controller/boot/SxpControllerInstanceTest.java
+++ sxp-controller/src/test/java/org/opendaylight/sxp/controller/boot/SxpControllerInstanceTest.java
@@ -8,22 +8,33 @@
 package org.opendaylight.sxp.controller.boot;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
+import java.util.Optional;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.opendaylight.mdsal.binding.api.ClusteredDataTreeChangeListener;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.api.ReadTransaction;
 import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.mdsal.binding.api.TransactionChain;
+import org.opendaylight.mdsal.binding.api.TransactionChainListener;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
@@ -43,34 +54,43 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.controller.rev141002.Sx
 import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.protocol.rev141002.NodeId;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yangtools.util.concurrent.FluentFutures;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({DatastoreAccess.class})
 public class SxpControllerInstanceTest {
 
     private static final InstanceIdentifier<BindingOrigins> BINDING_ORIGINS = InstanceIdentifier
             .builder(BindingOrigins.class).build();
 
-    private DatastoreAccess datastoreAccess;
-    private SxpControllerInstance controllerInstance;
+    private static BindingOrigins origins;
+
+    @Mock
+    private ClusterSingletonServiceProvider serviceProvider;
+    @Mock
     private ClusterSingletonServiceRegistration serviceRegistration;
-    private SxpDatastoreNode node;
-    private DataBroker dataBroker;
+    @Mock
     private ListenerRegistration listenerRegistration;
+    @Mock
+    private ReadTransaction readTransaction;
+    @Mock
+    private WriteTransaction writeTransaction;
+    @Mock
+    private SxpDatastoreNode node;
+    @Mock
+    private DataBroker dataBroker;
+    @Mock
     private RpcProviderService rpcProviderService;
+    @Mock
     private ObjectRegistration<SxpRpcServiceImpl> sxpRpcServiceRegistration;
+    @Mock
     private ObjectRegistration<SxpConfigRpcServiceImpl> sxpConfigRpcServiceRegistration;
 
-    @Before
-    public void init() {
-        PowerMockito.mockStatic(DatastoreAccess.class);
-        datastoreAccess = mock(DatastoreAccess.class);
-        PowerMockito.when(DatastoreAccess.getInstance(any(DataBroker.class))).thenReturn(datastoreAccess);
+    private DatastoreAccess datastoreAccess;
+    private SxpControllerInstance controllerInstance;
+
+    @BeforeClass
+    public static void setUpBindings() throws Exception {
         BindingOrigin local = new BindingOriginBuilder()
                 .setOrigin(BindingOriginsConfig.LOCAL_ORIGIN)
                 .setPriority((short) 1)
@@ -79,59 +99,63 @@ public class SxpControllerInstanceTest {
                 .setOrigin(BindingOriginsConfig.NETWORK_ORIGIN)
                 .setPriority((short) 2)
                 .build();
-        BindingOrigins origins = new BindingOriginsBuilder()
+        SxpControllerInstanceTest.origins = new BindingOriginsBuilder()
                 .setBindingOrigin(Lists.newArrayList(local, network))
                 .build();
-        when(datastoreAccess.readSynchronous(eq(BINDING_ORIGINS), eq(LogicalDatastoreType.CONFIGURATION)))
-                .thenReturn(origins);
-        serviceRegistration = mock(ClusterSingletonServiceRegistration.class);
-        ClusterSingletonServiceProvider serviceProvider = mock(ClusterSingletonServiceProvider.class);
-        when(serviceProvider.registerClusterSingletonService(any(ClusterSingletonService.class))).thenReturn(
-                serviceRegistration);
-        dataBroker = mock(DataBroker.class);
-        listenerRegistration = mock(ListenerRegistration.class);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        BindingOriginsConfig.INSTANCE.deleteConfiguration();
+    }
+
+    @Before
+    public void init() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(serviceProvider.registerClusterSingletonService(any(ClusterSingletonService.class)))
+                .thenReturn(serviceRegistration);
         when(dataBroker.registerDataTreeChangeListener(any(DataTreeIdentifier.class),
                 any(ClusteredDataTreeChangeListener.class))).thenReturn(listenerRegistration);
-        rpcProviderService = mock(RpcProviderService.class);
-        sxpRpcServiceRegistration = mock(ObjectRegistration.class);
-        sxpConfigRpcServiceRegistration = mock(ObjectRegistration.class);
         when(rpcProviderService.registerRpcImplementation(eq(SxpControllerService.class), any(SxpRpcServiceImpl.class)))
                 .thenReturn(sxpRpcServiceRegistration);
         when(rpcProviderService.registerRpcImplementation(eq(SxpConfigControllerService.class), any(SxpConfigRpcServiceImpl.class)))
                 .thenReturn(sxpConfigRpcServiceRegistration);
+
         controllerInstance = new SxpControllerInstance();
         controllerInstance.setClusteringServiceProvider(serviceProvider);
         controllerInstance.setDataBroker(dataBroker);
         controllerInstance.setRpcProviderService(rpcProviderService);
         controllerInstance.init();
-        node = mock(SxpDatastoreNode.class);
+
         when(node.getNodeId()).thenReturn(new NodeId("1.1.1.1"));
         Configuration.register(node);
+
+        datastoreAccess = prepareDataStore(dataBroker, readTransaction, writeTransaction);
     }
 
     @Test
     public void initTopology_1() throws Exception {
         SxpControllerInstance.initTopology(datastoreAccess, LogicalDatastoreType.CONFIGURATION);
-        verify(datastoreAccess, times(2)).merge(any(InstanceIdentifier.class), any(DataObject.class),
-                eq(LogicalDatastoreType.CONFIGURATION));
+        verify(writeTransaction, times(2))
+                .merge(eq(LogicalDatastoreType.CONFIGURATION), any(InstanceIdentifier.class), any(DataObject.class));
     }
 
     @Test
     public void initTopology_2() throws Exception {
         SxpControllerInstance.initTopology(datastoreAccess, LogicalDatastoreType.OPERATIONAL);
-        verify(datastoreAccess, times(2)).merge(any(InstanceIdentifier.class), any(DataObject.class),
-                eq(LogicalDatastoreType.OPERATIONAL));
+        verify(writeTransaction, times(2))
+                .merge(eq(LogicalDatastoreType.OPERATIONAL), any(InstanceIdentifier.class), any(DataObject.class));
     }
 
     @Test
     public void instantiateServiceInstance() throws Exception {
         controllerInstance.instantiateServiceInstance();
-        verify(datastoreAccess, times(3)).putIfNotExists(any(InstanceIdentifier.class), any(DataObject.class),
-                eq(LogicalDatastoreType.CONFIGURATION));
-        verify(datastoreAccess, times(2)).merge(any(InstanceIdentifier.class), any(DataObject.class),
-                eq(LogicalDatastoreType.CONFIGURATION));
-        verify(datastoreAccess, times(2)).merge(any(InstanceIdentifier.class), any(DataObject.class),
-                eq(LogicalDatastoreType.OPERATIONAL));
+        verify(writeTransaction, never())
+                .put(eq(LogicalDatastoreType.CONFIGURATION), eq(BINDING_ORIGINS), any());
+        verify(writeTransaction, times(2))
+                .merge(eq(LogicalDatastoreType.CONFIGURATION), any(InstanceIdentifier.class), any(DataObject.class));
+        verify(writeTransaction, times(2))
+                .merge(eq(LogicalDatastoreType.OPERATIONAL), any(InstanceIdentifier.class), any(DataObject.class));
         verify(dataBroker, atLeastOnce()).registerDataTreeChangeListener(any(DataTreeIdentifier.class),
                 any(ClusteredDataTreeChangeListener.class));
         verify(rpcProviderService).registerRpcImplementation(
@@ -147,7 +171,6 @@ public class SxpControllerInstanceTest {
 
         verify(listenerRegistration, atLeastOnce()).close();
         verify(node, atLeastOnce()).close();
-        verify(datastoreAccess, atLeastOnce()).close();
         verify(sxpRpcServiceRegistration).close();
         verify(sxpConfigRpcServiceRegistration).close();
     }
@@ -165,8 +188,40 @@ public class SxpControllerInstanceTest {
         verify(listenerRegistration, atLeastOnce()).close();
         verify(node, atLeastOnce()).close();
         verify(serviceRegistration).close();
-        verify(datastoreAccess, atLeastOnce()).close();
         verify(sxpRpcServiceRegistration).close();
         verify(sxpConfigRpcServiceRegistration).close();
+    }
+
+    /**
+     * Prepare {@link DatastoreAccess} mock instance backed by {@link DataBroker} for tests.
+     * <p>
+     * {@link ReadTransaction} and {@link WriteTransaction} are assumed to be created by {@link TransactionChain}.
+     * <p>
+     * {@link ReadTransaction} reads an mock instance of {@link DataObject} on any read and origins
+     * on read specifically on binding origins.
+     * {@link WriteTransaction} is committed successfully.
+     *
+     * @param dataBroker mock of {@link DataBroker}
+     * @param readTransaction mock of {@link ReadTransaction}
+     * @param writeTransaction mock of {@link WriteTransaction}
+     * @return mock of {@link DatastoreAccess}
+     */
+    private static DatastoreAccess prepareDataStore(DataBroker dataBroker, ReadTransaction readTransaction,
+            WriteTransaction writeTransaction) {
+        TransactionChain transactionChain = mock(TransactionChain.class);
+        doReturn(CommitInfo.emptyFluentFuture())
+                .when(writeTransaction).commit();
+        when(readTransaction.read(any(LogicalDatastoreType.class), any(InstanceIdentifier.class)))
+                .thenReturn(FluentFutures.immediateFluentFuture(Optional.of(mock(DataObject.class))));
+        when(readTransaction.read(eq(LogicalDatastoreType.CONFIGURATION), eq(BINDING_ORIGINS)))
+                .thenReturn(FluentFutures.immediateFluentFuture(Optional.of(SxpControllerInstanceTest.origins)));
+        when(transactionChain.newReadOnlyTransaction())
+                .thenReturn(readTransaction);
+        when(transactionChain.newWriteOnlyTransaction())
+                .thenReturn(writeTransaction);
+        when(dataBroker.createTransactionChain(any(TransactionChainListener.class)))
+                .thenReturn(transactionChain);
+
+        return DatastoreAccess.getInstance(dataBroker);
     }
 }
