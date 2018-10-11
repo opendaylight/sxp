2f24de35e1268edf7a23eaa3481e52c80b1ec6ff
Ivan Hrasko <ihrasko@cisco.com>
2018-10-11 15:45:27 +0200

    Do not check for parent existence
    - it is a responsibility of the caller to
    know/ensure that parent node exists
    
    - reduces unnecessary reads when writting
    
    - solves problem after Neon platform update
    when list parents are not present when
    inserting the first list item
    
    Change-Id: Ie725e1b20562db08907c2f6cdf05b3d1df61485d
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- sxp-controller/src/test/java/org/opendaylight/sxp/controller/listeners/sublisteners/FilterListenerTest.java
+++ sxp-controller/src/test/java/org/opendaylight/sxp/controller/listeners/sublisteners/FilterListenerTest.java
@@ -206,7 +206,6 @@ public class FilterListenerTest {
         doReturn(CommitInfo.emptyFluentFuture())
                 .when(writeTransaction).commit();
         when(readTransaction.read(any(LogicalDatastoreType.class), any(InstanceIdentifier.class)))
-                .thenReturn(FluentFutures.immediateFluentFuture(Optional.of(mock(DataObject.class))))
                 .thenReturn(FluentFutures.immediateFluentFuture(Optional.empty()))
                 .thenReturn(FluentFutures.immediateFluentFuture(Optional.of(mock(DataObject.class))));
         when(transactionChain.newReadOnlyTransaction())
