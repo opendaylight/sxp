dd101109108b1461820afda0b839927fea5fcc53
Ivan Hrasko <ihrasko@cisco.com>
2018-10-18 14:33:17 +0200

    Register RPC services on cluster owner only
    - some of the RPC calls depend on static structures
    which are available only on cluster owner
    
    - for example: map of sxp nodes, map of binding origins
    
    JIRA: SXP-145
    Change-Id: If34f44686a4e3c5318359eea909beabaeac31bcd
    Signed-off-by: Ivan Hrasko <ihrasko@cisco.com>

--- sxp-controller/src/main/java/org/opendaylight/sxp/controller/boot/SxpControllerInstance.java
+++ sxp-controller/src/main/java/org/opendaylight/sxp/controller/boot/SxpControllerInstance.java
@@ -15,12 +15,15 @@ import java.util.List;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
 import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
 import org.opendaylight.sxp.controller.core.DatastoreAccess;
 import org.opendaylight.sxp.controller.core.SxpDatastoreNode;
+import org.opendaylight.sxp.controller.core.SxpRpcServiceImpl;
 import org.opendaylight.sxp.controller.listeners.NodeIdentityListener;
 import org.opendaylight.sxp.controller.listeners.sublisteners.ConnectionTemplateListener;
 import org.opendaylight.sxp.controller.listeners.sublisteners.ConnectionsListener;
@@ -30,6 +33,7 @@ import org.opendaylight.sxp.controller.listeners.sublisteners.FilterListener;
 import org.opendaylight.sxp.controller.listeners.sublisteners.MasterDatabaseListener;
 import org.opendaylight.sxp.controller.listeners.sublisteners.PeerGroupListener;
 import org.opendaylight.sxp.core.Configuration;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.sxp.controller.rev141002.SxpControllerService;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopologyBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
@@ -41,26 +45,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SxpControllerInstance implements ClusterSingletonService, AutoCloseable {
-
-    protected static final Logger LOG = LoggerFactory.getLogger(SxpControllerInstance.class);
-
     public static final ServiceGroupIdentifier IDENTIFIER =
             ServiceGroupIdentifier.create(SxpControllerInstance.class.getName());
+
+    private static final Logger LOG = LoggerFactory.getLogger(SxpControllerInstance.class);
+
+    private final List<ListenerRegistration<DataTreeChangeListener>> dataChangeListenerRegistrations = new ArrayList<>();
 
     private DataBroker dataBroker;
     private ClusterSingletonServiceProvider clusteringServiceProvider;
     private DatastoreAccess datastoreAccess;
     private ClusterSingletonServiceRegistration clusterServiceRegistration;
-    private final List<ListenerRegistration<DataTreeChangeListener>>
-            dataChangeListenerRegistrations = new ArrayList<>();
+    private RpcProviderRegistry rpcProviderRegistry;
+    private RpcRegistration<SxpControllerService> sxpRpcServiceRegistration;
 
     public void init() {
         Preconditions.checkNotNull(dataBroker);
         Preconditions.checkNotNull(clusteringServiceProvider);
+        Preconditions.checkNotNull(rpcProviderRegistry);
         LOG.info("Registering into singleton clustering service");
         this.clusterServiceRegistration =
                 clusteringServiceProvider.registerClusterSingletonService(this);
         LOG.info("Clustering session initiated for {}", this.getClass().getSimpleName());
+    }
+
+    private void registerRpcServices() {
+        sxpRpcServiceRegistration = rpcProviderRegistry.addRpcImplementation(
+                SxpControllerService.class, new SxpRpcServiceImpl(dataBroker));
     }
 
     static synchronized void initTopology(final DatastoreAccess datastoreAccess,
@@ -78,6 +89,11 @@ public class SxpControllerInstance implements ClusterSingletonService, AutoClose
     public synchronized void instantiateServiceInstance() {
         LOG.warn("Instantiating {}", this.getClass().getSimpleName());
         this.datastoreAccess = DatastoreAccess.getInstance(dataBroker);
+
+        // register RPC services to be running on cluster owner only
+        registerRpcServices();
+
+        // create listener
         NodeIdentityListener datastoreListener = new NodeIdentityListener(datastoreAccess);
         //noinspection unchecked
         datastoreListener.addSubListener(
@@ -95,10 +111,15 @@ public class SxpControllerInstance implements ClusterSingletonService, AutoClose
         dataChangeListenerRegistrations.add(datastoreListener.register(dataBroker, LogicalDatastoreType.OPERATIONAL));
     }
 
+    private void closeRpcServices() {
+        sxpRpcServiceRegistration.close();
+    }
+
     @Override
     public synchronized ListenableFuture<Void> closeServiceInstance() {
         LOG.warn("Clustering provider closed service for {}", this.getClass().getSimpleName());
-        dataChangeListenerRegistrations.forEach(ListenerRegistration<DataTreeChangeListener>::close);
+        closeRpcServices();
+        dataChangeListenerRegistrations.forEach(ListenerRegistration::close);
         dataChangeListenerRegistrations.clear();
         Configuration.getNodes().forEach(n -> {
             if (n instanceof SxpDatastoreNode) {
@@ -125,20 +146,15 @@ public class SxpControllerInstance implements ClusterSingletonService, AutoClose
         }
     }
 
-    public DataBroker getDataBroker() {
-        return dataBroker;
-    }
-
     public void setDataBroker(DataBroker dataBroker) {
         this.dataBroker = dataBroker;
-    }
-
-    public ClusterSingletonServiceProvider getClusteringServiceProvider() {
-        return clusteringServiceProvider;
     }
 
     public void setClusteringServiceProvider(ClusterSingletonServiceProvider clusteringServiceProvider) {
         this.clusteringServiceProvider = clusteringServiceProvider;
     }
 
+    public void setRpcProviderRegistry(RpcProviderRegistry rpcProviderRegistry) {
+        this.rpcProviderRegistry = rpcProviderRegistry;
+    }
 }
